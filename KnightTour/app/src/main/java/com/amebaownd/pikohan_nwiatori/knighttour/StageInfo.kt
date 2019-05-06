e1b025b817fb2fef1ca9bbfd65177ab1aa524e69
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-06 20:46:50 +0900

    複合キーに変更

--- KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/StageInfo.kt
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/StageInfo.kt
@@ -3,12 +3,12 @@ package com.amebaownd.pikohan_nwiatori.knighttour
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
-@Entity
+@Entity(primaryKeys = arrayOf("stageId","row","column"))
 class StageInfo{
-    @PrimaryKey
+
     var stageId:Int=0
-    @PrimaryKey
+
     var row:Int=1
-    @PrimaryKey
+
     var column:Int=1
 }
