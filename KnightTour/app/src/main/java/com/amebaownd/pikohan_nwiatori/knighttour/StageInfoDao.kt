847cc887b4e82c6a75f64a9de0f7fbd7b53034d1
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-06 16:51:37 +0900

    stage_infoのquery

---
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/StageInfoDao.kt
@@ -0,0 +1,18 @@
+package com.amebaownd.pikohan_nwiatori.knighttour
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+@Dao
+interface StageInfoDao{
+    @Query("SELECT * FROM StageInfo ORDER BY stageId,`row`,`column`")
+    fun getAll():LiveData<List<StageInfo>>
+
+    @Query("SELECT * FROM StageInfo WHERE stageId=(:stageId) ORDER BY `row`,`column`")
+    fun getByStageId(stageId:Int):LiveData<List<StageInfo>>
+
+    @Insert
+    fun insertAll(vararg stageInfo:StageInfo)
+}
