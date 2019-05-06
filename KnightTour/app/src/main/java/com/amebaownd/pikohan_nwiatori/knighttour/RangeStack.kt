7956c869494423b82564c6b887d3be10806c57a8
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-06 16:50:14 +0900

    座標用のスタックを追加

---
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/RangeStack.kt
@@ -0,0 +1,22 @@
+package com.amebaownd.pikohan_nwiatori.knighttour
+
+data class Range(val row:Int,val column:Int)
+
+class RangeStack(){
+    var mutableList = mutableListOf<Range>()
+
+    fun push(range:Range){
+        mutableList.add(range)
+    }
+
+    fun pop():Range{
+        val result =mutableList[mutableList.lastIndex]
+        mutableList.removeAt(mutableList.lastIndex)
+        return result
+    }
+    fun isEmpty() = mutableList.isEmpty()
+
+    fun isUnique(range:Range)=mutableList.contains(range)
+
+    fun size() = mutableList.size
+}
