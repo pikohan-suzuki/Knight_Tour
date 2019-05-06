ab01fe7e3ab61a0b23518fe8f11839f5530d1fb3
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-06 16:50:35 +0900

    record用のテーブルを追加

---
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/Record.kt
@@ -0,0 +1,14 @@
+package com.amebaownd.pikohan_nwiatori.knighttour
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
+class Record{
+    @PrimaryKey
+    var stageId:Int=0
+
+    var time:Int=9999
+
+    var rank:String=""
+}
