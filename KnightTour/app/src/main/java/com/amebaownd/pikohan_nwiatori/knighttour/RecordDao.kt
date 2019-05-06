e20b72b2b35d5d0e95cc5f3959e281a6e5b7bbd7
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-06 16:50:51 +0900

    record_tableのquery

---
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/RecordDao.kt
@@ -0,0 +1,18 @@
+package com.amebaownd.pikohan_nwiatori.knighttour
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+@Dao
+interface  RecordDao{
+    @Query("SELECT * FROM record ORDER BY stageId")
+    fun getAllOrderByStageId():LiveData<List<Record>>
+
+    @Query("SELECT * FROM record WHERE stageId = (:stageId)")
+    fun getByStageId(stageId:Int):LiveData<Record>
+
+    @Insert
+    fun insertAll(vararg record:Record)
+}
