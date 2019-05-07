168b623a25f8253caaec36c81c21bef71819f643
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-07 20:42:52 +0900

    stage情報を読み込む処理を追加

--- KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/MainActivity.kt
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/MainActivity.kt
@@ -5,8 +5,14 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.InputStreamReader
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,13 +22,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nextStage = readFile(this,"nextStage")?.toInt() ?: 1
+        nextStage = readFile(this, "nextStage")?.toInt() ?: 1
         stageTextVew = findViewById(R.id.stage_id_front_textView)
         stageTextVew.text = getString(R.string.stage_id_front, nextStage)
 
         val privacyPolicyTextView = findViewById<TextView>(R.id.privacy_policy_textView)
-        privacyPolicyTextView.text= Html.fromHtml("<a href=\"https://pikohan-niwatori.amebaownd.com/posts/6171216\">PrivacyPolicy</a>")
-        privacyPolicyTextView.linksClickable=true
+        privacyPolicyTextView.text =
+            Html.fromHtml("<a href=\"https://pikohan-niwatori.amebaownd.com/posts/6171216\">PrivacyPolicy</a>")
+        privacyPolicyTextView.linksClickable = true
         val startBeginButton = findViewById<Button>(R.id.from_beginning_button)
         startBeginButton.setOnClickListener {
             val intent = Intent(this, GameActivity::class.java)
@@ -45,14 +52,37 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("stage_id", nextStage)
             startActivity(intent)
         }
+
+        readCsv()
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        if (intent!=null && requestCode == 101 && resultCode == Activity.RESULT_OK) {
+        if (intent != null && requestCode == 101 && resultCode == Activity.RESULT_OK) {
             nextStage = intent.getIntExtra("stage_id", 1)
             stageTextVew.text = getString(R.string.stage_id_front, nextStage)
         }
     }
 
-
+    private fun readCsv() :List<Stage>{
+        val inputStream = resources.assets.open("stage.csv")
+        val inputStreamReader = InputStreamReader(inputStream)
+        val results = mutableListOf<Stage>()
+        inputStreamReader.use {
+            it.readLines().forEach{
+                val str = it!!.split(",")
+                val result = Stage().apply {
+                    this.stageId=str[0].toInt()
+                    this.row=str[1].toInt()
+                    this.column=str[2].toInt()
+                    this.sTime=str[3].toInt()
+                    this.aTime=str[4].toInt()
+                    this.sTime=str[5].toInt()
+                    this.aTime=str[6].toInt()
+                }
+                results.add(result)
+            }
+        }
+        return results.toList()
+    }
 }
