4ec59cb07df043dd5656a545774a9cc8cdfd6a31
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-15 18:50:14 +0900

    updateを追加

--- KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/Data/RecordDao.kt
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/Data/RecordDao.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
+import android.arch.persistence.room.Update
 
 @Dao
 interface  RecordDao{
@@ -15,4 +16,7 @@ interface  RecordDao{
 
     @Insert
     fun insertAll(vararg record: Record)
+
+    @Update
+    fun update(record:Record)
 }
