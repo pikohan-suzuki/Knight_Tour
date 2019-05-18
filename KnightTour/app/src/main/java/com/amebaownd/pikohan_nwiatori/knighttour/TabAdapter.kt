bd844282387a5e49f55e7e514c852d61622c0c9c
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-18 18:06:21 +0900

    タブ数の変更

--- KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/TabAdapter.kt
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/TabAdapter.kt
@@ -1,24 +1,32 @@
 package com.amebaownd.pikohan_nwiatori.knighttour
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.support.constraint.ConstraintLayout
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.*
 import android.widget.TextView
 
 class TabAdapter(fm: FragmentManager, private val context: Context) : FragmentStatePagerAdapter(fm) {
 
     private val inflater = LayoutInflater.from(context)
-    private val title = arrayOf<String>("1", "2")
+    private val title = arrayOf<String>("1", "2","3","4")
     override fun getItem(position: Int): Fragment? {
         when(position){
             0->
                 return InfoFragment1()
             1->
                 return InfoFragment2()
+            2->
+                return InfoFragment3()
+            3->
+                return InfoFragment4()
         }
         return InfoFragment1()
     }
@@ -31,6 +39,7 @@ class TabAdapter(fm: FragmentManager, private val context: Context) : FragmentSt
 
     fun getTabView(tabLayout: TabLayout, position: Int): View {
         val view = inflater.inflate(R.layout.tab_item, tabLayout, false)
+//        view.setBackgroundColor(Color.RED)
         val tabTextView = view.findViewById<TextView>(R.id.tab_item_textView)
         tabTextView.text = getPageTitle(position)
         tabTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
