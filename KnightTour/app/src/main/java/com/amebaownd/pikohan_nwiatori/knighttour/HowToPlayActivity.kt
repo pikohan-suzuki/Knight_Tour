42347d2229b031384ba939300a399790fbc20cb4
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-18 18:03:35 +0900

    タブボタンの追加

--- KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/HowToPlayActivity.kt
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/HowToPlayActivity.kt
@@ -1,59 +1,95 @@
 package com.amebaownd.pikohan_nwiatori.knighttour
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TableLayout
+import kotlinx.android.synthetic.main.activity_how_to_play.*
 
 class HowToPlayActivity() : AppCompatActivity() {
 
     lateinit var adapter: TabAdapter
-    lateinit var tabLayout: TabLayout
-    lateinit var tab: TabLayout.Tab
-    private val TAB_SIZE = 2
+    private var currentPage = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_how_to_play)
 
         setTabLayout()
 
-        findViewById<Button>(R.id.back_how_to_play_button)
+        findViewById<ImageButton>(R.id.back_how_to_play_button)
             .setOnClickListener {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
-        findViewById<Button>(R.id.next_button)
+        findViewById<ImageButton>(R.id.next_button)
             .setOnClickListener {
                 nextTab()
             }
-        findViewById<Button>(R.id.previous_button)
+        findViewById<ImageButton>(R.id.previous_button)
             .setOnClickListener {
                 previousTab()
             }
+        findViewById<ImageView>(R.id.red_tab_imageButton).setOnClickListener(setTabClickListener(findViewById<ImageView>(R.id.red_tab_imageButton)))
+        findViewById<ImageView>(R.id.blue_tab_imageButton).setOnClickListener(setTabClickListener(findViewById<ImageView>(R.id.blue_tab_imageButton)))
+        findViewById<ImageView>(R.id.green_tab_imageButton).setOnClickListener(setTabClickListener(findViewById<ImageView>(R.id.green_tab_imageButton)))
+        findViewById<ImageView>(R.id.orange_tab_imageButton).setOnClickListener(setTabClickListener(findViewById<ImageView>(R.id.orange_tab_imageButton)))
     }
 
     private fun setTabLayout() {
         adapter = TabAdapter(supportFragmentManager, this)
-        tabLayout = findViewById<TabLayout>(R.id.tab_layout)
         val viewPager = findViewById<ViewPager>(R.id.viewPager)
         viewPager.adapter = adapter
-        tabLayout.setupWithViewPager(viewPager)
-        tab = tabLayout.getTabAt(0)!!
-        tab.customView = adapter.getTabView(tabLayout, 0)
     }
 
     private fun nextTab() {
-        if (tabLayout.selectedTabPosition < tabLayout.tabCount-1) {
-            tabLayout.getTabAt(tabLayout.selectedTabPosition+1)!!.select()
+        if (currentPage < viewPager.childCount ) {
+            currentPage += 1
+            viewPager.setCurrentItem(currentPage)
         }
     }
 
     private fun previousTab() {
-        if (tabLayout.selectedTabPosition > 0) {
-            tabLayout.getTabAt(tabLayout.selectedTabPosition-1)!!.select()
+        if (currentPage > 0) {
+            currentPage -= 1
+            viewPager.setCurrentItem(currentPage)
         }
+    }
+
+    fun setTabClickListener(view: View): View.OnClickListener? {
+        when (view.id) {
+            R.id.red_tab_imageButton -> {
+                return View.OnClickListener {
+                    currentPage = 0
+                    viewPager.setCurrentItem(currentPage)
+                }
+            }
+            R.id.blue_tab_imageButton -> {
+                return View.OnClickListener {
+                    currentPage = 1
+                    viewPager.setCurrentItem(currentPage)
+                }
+            }
+            R.id.green_tab_imageButton -> {
+                return View.OnClickListener {
+                    currentPage = 2
+                    viewPager.setCurrentItem(currentPage)
+                }
+            }
+            R.id.orange_tab_imageButton -> {
+                return View.OnClickListener {
+                    currentPage = 3
+                    viewPager.setCurrentItem(currentPage)
+                }
+            }
+        }
+        return null
     }
 }
