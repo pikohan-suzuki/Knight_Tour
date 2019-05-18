e0e654558ddb3a67419584123529db10fb8ef5d5
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-18 18:04:53 +0900

    bind処理を追加

--- KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/Dialog/CongratulationDialog.kt
+++ KnightTour/app/src/main/java/com/amebaownd/pikohan_nwiatori/knighttour/Dialog/CongratulationDialog.kt
@@ -7,22 +7,33 @@ import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.amebaownd.pikohan_nwiatori.knighttour.MainActivity
 import com.amebaownd.pikohan_nwiatori.knighttour.R
+import com.amebaownd.pikohan_nwiatori.knighttour.StageSelectActivity
+import java.sql.Time
+import java.text.SimpleDateFormat
 
-class CongratulationDialog: DialogFragment() {
+class CongratulationDialog : DialogFragment() {
+    var stageId: Int = 1
+    var rank: String = ""
+    var time: Time? = null
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val alertDialog : View = activity!!.layoutInflater.inflate(R.layout.dialog_congratulation,null)
-        val okButton = alertDialog.findViewById<Button>(R.id.ok_congratulation_button)
-        okButton.setOnClickListener{
+        val alertDialog: View = activity!!.layoutInflater.inflate(R.layout.dialog_congratulation, null)
+        val stageSelectButton = alertDialog.findViewById<Button>(R.id.stage_select_congratulation_button)
+        stageSelectButton.setOnClickListener {
             this.dismiss()
-            val intent = Intent(this.context, MainActivity::class.java)
+            val intent = Intent(this.context, StageSelectActivity::class.java)
             startActivity(intent)
         }
+        val rankTextView = alertDialog.findViewById<TextView>(R.id.congratulation_rank)
+        rankTextView.text = getString(R.string.rank, rank)
+        val timeTextView = alertDialog.findViewById<TextView>(R.id.congratulation_clear_time)
+        timeTextView.text = "Time: " + SimpleDateFormat("mm:ss").format(time)
 
         val dialogBuilder = AlertDialog.Builder(activity)
         dialogBuilder.setView(alertDialog)
-        this.isCancelable=false
+        this.isCancelable = false
         return dialogBuilder.create()
     }
 }
